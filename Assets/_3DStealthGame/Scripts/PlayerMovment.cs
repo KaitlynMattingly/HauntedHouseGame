75a889f05056405bac32dd0f56b5c0cad4ef2d33
KaitlynMattingly <kaitlynmattingy@gmail.com>
2025-12-03 21:03:40 -0500

    Update

---
+++ Assets/_3DStealthGame/Scripts/PlayerMovment.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public InputAction MoveAction;
+
+    public float walkSpeed = 1.0f;
+    public float turnSpeed = 20f;
+
+    public float sprintSpeed = 2.5f;
+    public float maxSprintTime = 2f;
+    private float sprintTimer;
+    private bool canSprint = true;
+    public float sprintRechargeTime = 3f;
+    private float rechargeTimer = 0f;
+    public bool CanSprint => canSprint;
+
+    private bool isFrozen = false;
+    private float freezeTimer = 0f;
+    public float minFreezeInterval = 8f;
+    public float maxFreezeInterval = 12f;
+    public float freezeDuration = 3f;
+    public bool IsFrozen => isFrozen;
+    private IEnumerator FreezePlayer()
+    {
+
+        yield return new WaitForSeconds(freezeDuration);
+        isFrozen = false;
+    }
+
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    Animator m_Animator;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        MoveAction.Enable();
+        m_Animator = GetComponent<Animator>();
+
+        sprintTimer = maxSprintTime;
+
+        freezeTimer = Random.Range(minFreezeInterval, maxFreezeInterval);
+    }
+
+    void FixedUpdate()
+    {
+
+        if (!isFrozen)
+        {
+            freezeTimer -= Time.deltaTime;
+            if (freezeTimer <= 0f)
+            {
+                isFrozen = true;
+                StartCoroutine(FreezePlayer());
+                freezeTimer = Random.Range(minFreezeInterval, maxFreezeInterval);
+            }
+        }
+
+        if (isFrozen)
+            return;
+
+        var pos = MoveAction.ReadValue<Vector2>();
+        float horizontal = pos.x;
+        float vertical = pos.y;
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        float currentSpeed = walkSpeed;
+        bool sprintHeld = Keyboard.current.leftShiftKey.isPressed;
+        if (sprintHeld && canSprint && m_Movement.magnitude > 0f)
+        {
+            currentSpeed = sprintSpeed;
+            sprintTimer -= Time.deltaTime;
+            if (sprintTimer <= 0f)
+            {
+                canSprint = false;
+                rechargeTimer = sprintRechargeTime;
+            }
+        }
+        else
+        {
+            if (!canSprint)
+            {
+                rechargeTimer -= Time.deltaTime;
+                if (rechargeTimer <= 0f)
+                {
+                    canSprint = true;
+                    sprintTimer = maxSprintTime;
+                }
+            }
+        }
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+
+        m_Rigidbody.MoveRotation(m_Rotation);
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * currentSpeed * Time.deltaTime);
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+    }
+}
---
+++ Assets/_3DStealthGame/Scripts/SprintFrozenUI.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SprintFrozenUI : MonoBehaviour
+{
+    public PlayerMovement playerMovement;
+
+    public GameObject SprintOn;         
+    public GameObject SprintOff;
+    public GameObject FrozenOn;
+    public GameObject FrozenOff;
+
+
+    void Update()
+    {
+        if (playerMovement == null) return;
+
+        if (playerMovement.CanSprint)
+        {
+            SprintOn.SetActive(true);
+            SprintOff.SetActive(false);
+        }
+        else
+        {
+            SprintOn.SetActive(false);
+            SprintOff.SetActive(true);
+        }
+        if (playerMovement.IsFrozen)
+        {
+            FrozenOn.SetActive(true);
+            FrozenOff.SetActive(false);
+        }
+        else
+        {
+            FrozenOn.SetActive(false);
+            FrozenOff.SetActive(true);
+        }
+    }
+}
